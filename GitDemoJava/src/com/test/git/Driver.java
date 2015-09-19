407205a1e979c6d254335c1920670d38a85c004e
RajkumarJain <RajkumarJain@LP7679.people.local>
2015-09-19 12:12:29 +0530

    Initial checkin of Git Demo..

---
+++ GitDemoJava/src/com/test/git/Driver.java
@@ -0,0 +1,17 @@
+package com.test.git;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		
+		Functions func=new Functions();
+		display();
+		func.function1();
+	}
+	
+	public static void display(){
+		System.out.println("THIS IS LINE 1 IN DRIVER");
+		System.out.println("THIS IS LINE 2 IN DRIVER");
+	}
+
+}
---
+++ GitDemoJava/src/com/test/git/Functions.java
@@ -0,0 +1,9 @@
+package com.test.git;
+
+public class Functions {
+	public void function1(){
+		String name="RJ";
+		System.out.println("MY NAME IS "+name+" "+getClass().getName());
+	}
+
+}
