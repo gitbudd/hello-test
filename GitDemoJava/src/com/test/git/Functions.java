aaf530efc03e177df7b804da2a7d4c855a906e8f
RajkumarJain <RajkumarJain@LP7679.people.local>
2015-09-19 12:36:28 +0530

    Partial commit about display array data

--- GitDemoJava/src/com/test/git/Functions.java
+++ GitDemoJava/src/com/test/git/Functions.java
@@ -1,9 +1,11 @@
 package com.test.git;
 
 public class Functions {
+	int data[]={0,4,2,9};
 	public void function1(){
 		String name="RJ";
 		System.out.println("MY NAME IS "+name+" "+getClass().getName());
+		System.out.println("********************New Implementation*********************");
 	}
 
 }
